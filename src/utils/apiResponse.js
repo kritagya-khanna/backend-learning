e74b584b9e139a9514893d8f02ac74b42b111780
kritagya-khanna <kritagykhanna21k@gmail.com>
2024-09-16 15:26:12 +0530

    2nd day

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    consturctor(statuscode, data, message="Success"){
+        this.statusCode = this.statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode <400
+    }
+}
