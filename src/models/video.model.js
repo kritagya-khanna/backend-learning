a6b3230030dbecc3f413ff30119eaa99e219cdc2
kritagya-khanna <kritagykhanna21k@gmail.com>
2024-09-23 15:41:56 +0530

    data upload on cloudinary

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -3,7 +3,7 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 
 const videoSchema = new Schema({
-    videoFile:{
+    videoFile: {
         type: String,
         required: true,
     },
